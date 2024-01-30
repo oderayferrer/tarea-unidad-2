ee9ab3d7a0013f61a45684201b28bf9d022e96e6
oderayferrer <ferreroderay@gmail.com>
2024-01-30 18:44:12 -0300

    Create ejercicio3.java

---
+++ ejercicio3.java
@@ -0,0 +1,32 @@
+programa MostrarDiasDeLaSemana
+inicio
+    ingresar: nombreDiaUno
+    primerDia = nombreDiaUno
+
+    ingresar: nombreDiaDos
+    segundoDia = nombreDiaDos
+
+    ingresar: nombreDiaTres
+    tercerDia = nombreDiaTres
+
+    ingresar: nombreDiaCuatro
+    cuartoDia = nombreDiaCuatro
+
+    ingresar: nombreDiaCinco
+    quintoDia = nombreDiaCinco
+
+    ingresar: nombreDiaSeis
+    sextoDia = nombreDiaSeis
+
+    ingresar: nombreDiaSiete
+    septimoDia = nombreDiaSiete
+
+    mostrar: primerDia
+    mostrar: segundoDia
+    mostrar: tercerDia
+    mostrar: cuartoDia
+    mostrar: quintoDia
+    mostrar: sextoDia
+    mostrar: septimoDia
+fin
+
