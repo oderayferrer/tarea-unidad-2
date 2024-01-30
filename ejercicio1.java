3d4669e2a51a4709e3d7909bc08d287a1b26d7dd
oderayferrer <ferreroderay@gmail.com>
2024-01-30 18:16:36 -0300

    primer requerimiento

---
+++ ejercicio1.java
@@ -0,0 +1,10 @@
+programa PrimerPrograma
+inicio
+    ingresar: numero1
+    ingresar: numero2
+    primerResultado = numero1 + numero2
+    mostrar: primerResultado
+    ingresar: numero3 
+    segundoResultado = primerResultado + numero3
+    mostrar: segundoResultado
+fin
