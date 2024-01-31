af8edec92a9c5d42be6c75123253da824f2269b1
oderayferrer <ferreroderay@gmail.com>
2024-01-30 21:42:37 -0300

    ejercicio 19

---
+++ ejercicio19.java
@@ -0,0 +1,12 @@
+programa PorcentajeCualquiera
+inicio
+    mostrar: "Ingrese un numero"
+    ingresar: numero
+
+    mostrar: "Ingrese un porcentaje"
+    ingresar: porcentaje
+
+    resultado = numero * (porcentaje / 100)
+
+    mostrar: "El" + porcentaje + "%, de" + numero + "es" + resultado 
+fin 
