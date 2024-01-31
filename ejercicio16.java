3aadea1964cd02353e8c6be58047229b8e563ae2
oderayferrer <ferreroderay@gmail.com>
2024-01-30 21:27:07 -0300

    ejercicio 16

---
+++ ejercicio16.java
@@ -0,0 +1,11 @@
+programa ClaudiaApodoYEdad
+inicio
+    nombre = "Claudia" 
+    mostrar: "Hola" + nombre + "Ingrese su apodo"
+    ingresar: apodo
+
+    mostrar: apodo + "Ingrese su edad"
+    ingresar: edad
+
+    mostrar: apodo + "tiene" + edad + "años"
+fin  
