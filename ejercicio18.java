709e3a7ae2ae4427f7ecb6d45a755c2445f6295c
oderayferrer <ferreroderay@gmail.com>
2024-01-30 21:37:18 -0300

    EJERCICIO 18

---
+++ ejercicio18.java
@@ -0,0 +1,19 @@
+programa PromedioClub
+inicio
+    mostrar: "Ingrese el nombre del club"
+    ingresar: nombreClub
+
+    mostrar: "Ingrese los puntos de la primera temporada"
+    ingresar: puntosPrimeraTemporada
+
+    mostrar: "Ingrese los puntos de la segunda temporada"
+    ingresar: puntosSegundaTemporada
+
+    mostrar: "Ingrese los puntos de la tercera temporada"
+    ingresar: puntosTerceraTemporada
+
+    promedio = (puntosPrimeraTemporada + puntosSegundaTemporada + puntosTerceraTemporada) / 3 
+
+    mostrar: "El club" + nombreClub + "tiene un promedio de puntos de" + promedio
+
+fin
