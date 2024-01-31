26c5890f348ec9ca3060fac30ef26ffcf5ca5a5f
oderayferrer <ferreroderay@gmail.com>
2024-01-30 21:31:49 -0300

    ejercicio 17

---
+++ ejercicio17.java
@@ -0,0 +1,12 @@
+programa DescontarGastoMensualASueldo
+inicio
+    mostrar: "Ingrese el monto de su sueldo"
+    ingresar: montoSueldo
+
+    mostrar: "Ingrese el total de sus gastos mensuales"
+    ingresar: gastosMensuales
+
+    montoNeto = montoSueldo - gastosMensuales
+
+    mostrar: "El monto neto final es" + montoNeto 
+fin
