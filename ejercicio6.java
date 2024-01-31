a0a6ee2c215388ad1eb6fb8009760961c28933f0
oderayferrer <ferreroderay@gmail.com>
2024-01-30 22:12:10 -0300

    delete unused

--- ejercicio6.java
+++
@@ -1,8 +0,0 @@
-programa EmailMensaje
-inicio
-    mostrar: "Ingrese una direccion de correo electronico"
-    ingresar: direccionDeCorreoElectronico
-
-    mensaje = "este correo se envió al usuario"
-    mostrar: direccionDeCorreoElectronico + mensaje
-fin
